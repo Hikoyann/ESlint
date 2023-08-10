d894b4a9041914ad664e517e3b6d1d45008f55f6
Hikoyann <i21i022dl@ous.jp>
2023-08-10 23:52:17 +0900

    eslintrc.jsの書き換え
    parserOptions:
    plugins:
    extends:
    rules:
    などを書き換えて、学習しました。
    extendsとpackage.jsonとの繋がりがあるため、色々と修正が必要。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": "plugin:react/recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-        "react/prop-types": ["off"]
-    }
+  ],
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: [],
+  extends: ["plugin:react/recomended"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "react/prop-types": ["off"],
+    "react/jsx-props-no-spreading": "off",
+  },
 };
